296c1e298fe923d0805292c2b72d0309998eb44d
sunanda35 <sunandasamanta60@gmail.com>
2021-12-25 23:45:38 +0530

    update

--- chatee-backend/src/app.ts
+++ chatee-backend/src/app.ts
@@ -1,11 +1,35 @@
-import express from "express";
-const app = express();
-const port = 3000;
+import express, { Request, Response, NextFunction } from "express";
+import CreateHttpError from "http-errors";
+import morgan from "morgan";
+import cors from "cors";
 
-app.get("/", (req, res) => {
+const app = express();
+app.use(
+  express.json({
+    limit: "20mb",
+  })
+);
+app.use(cors());
+
+app.use(morgan(":method :url HTTP/:http-version :status :response-time"));
+
+const PORT = 3000;
+
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(CreateHttpError(400, "This is Wrong Route!"));
+});
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(err.status || 500).json({
+    status: err.status || 500,
+    message: err.message || "Something Went Wrong!",
+  });
+});
+
+app.listen(PORT, () => {
+  return console.log(`Server started at: http://localhost:${PORT}`);
 });
